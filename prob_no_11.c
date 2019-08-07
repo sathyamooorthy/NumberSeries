f24fdb2bdfcac28fcc576a707f05ea7f8ef10b36
sathyamooorthy <50096384+sathyamooorthy@users.noreply.github.com>
2019-08-07 20:14:32 +0530

    Create prob_no_11.c

---
+++ prob_no_11.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int n,a=2,i;
+    scanf("%d",&n);
+    for(i=1;i<10;i++)
+    {
+       if(i%2!=0)
+       {
+           n=n*a;
+       }
+       else
+       {
+           n=n+a;
+       }
+       a=a+2;
+       printf("%d ",n);
+    }
+}
